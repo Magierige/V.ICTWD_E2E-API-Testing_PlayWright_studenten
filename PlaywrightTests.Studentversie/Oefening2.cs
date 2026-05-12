20cf81b2fb7a0ec4ff1f80161eac6200717cc537
Ronald <s1208278@student.windesheim.nl>
2026-05-12 11:29:38 +0200

    oefening 2 en 3

---
+++ PlaywrightTests.Studentversie/Oefening2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace PlaywrightTests
+{
+    [TestClass]
+    public class Oefening2 : BasePageTest
+    {
+        [TestMethod]
+        public async Task CheckenAantalProducten()
+        {
+            var filter = new LocatorFilterOptions { Visible = true };
+            await Page.GotoAsync("/oefening2.html");
+            await Expect(Page.Locator(".product-card").Filter(filter)).ToHaveCountAsync(3);
+        }
+    }
+}
---
+++ PlaywrightTests.Studentversie/oefening3.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaywrightTests
+{
+    [TestClass]
+    public class Oefening3 : BasePageTest
+    {
+        //[TestMethod]
+        //public async Task inloggen()
+        //{
+        //    var nameInput = Page.GetByRole("input",{ name: "name" });
+        //    nameInput.InputValueAsync("John");
+        //    var submit Page.GetByRole('button', { name: 'Submit'});
+        //    await Expect(submit).Tohave(true);
+        //}
+        [TestMethod]
+        public async Task Inloggen()
+        {
+            await Page.GotoAsync("/oefening3.html");
+
+            await Page.GetByTestId("input-name").FillAsync("John");
+            await Page.GetByTestId("btn-submit").ClickAsync();
+            await Expect(Page.Locator("#signout-form")).ToContainTextAsync("Welkom John!");
+        }
+    }
+}
